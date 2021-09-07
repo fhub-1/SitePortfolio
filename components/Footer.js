3a131f9c071fd6dfeaf36ad79acd55f773271d43
fhub-1 <flutterhub1.21.0@gmail.com>
2021-09-07 14:54:52 +0300

    adding google anlystics

--- components/Footer.js
+++ components/Footer.js
@@ -15,7 +15,7 @@ export default function Footer() {
               Developed by
               <div className="inline-block transform hover:scale-110 hover:-rotate-3 transition duration-300">
                 <a
-                  className="hover:bg-indigo-500 rounded-md px-2 py-1  hover:text-gray-500"
+                  className="hover:bg-indigo-500 rounded-md px-2 py-1  hover:text-black"
                   href="https://manuarora.in"
                 >
                   Joseph Kitheka
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,26 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.of("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
   );
-}
+};
 
 export default MyApp;
