042afe20dea101a960fcf08a2abcb0c61642365f
fhub-1 <flutterhub1.21.0@gmail.com>
2021-09-07 13:25:40 +0300

    adding same file

--- components/Footer.js
+++ components/Footer.js
@@ -15,7 +15,7 @@ export default function Footer() {
               Developed by
               <div className="inline-block transform hover:scale-110 hover:-rotate-3 transition duration-300">
                 <a
-                  className="hover:bg-red-500 rounded-md px-2 py-1  hover:text-gray-500"
+                  className="hover:bg-indigo-500 rounded-md px-2 py-1  hover:text-gray-500"
                   href="https://manuarora.in"
                 >
                   Joseph Kitheka
