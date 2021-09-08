2bb4900bc67bb31ffb99d44133dbb6a5cee61d1c
fhub-1 <flutterhub1.21.0@gmail.com>
2021-09-08 11:25:01 +0300

    adding google any new

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
-import "../styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { ThemeProvider } from "next-themes";
+import * as gtag from "../lib/gtag";
 
-function MyApp({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
   );
-}
+};
 
-export default MyApp;
+export default App;
