959ae426c823f31b91378b3ec53237ec4ee7c284
fhub-1 <flutterhub1.21.0@gmail.com>
2021-09-08 14:05:05 +0300

    removing

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import CookieConsent from "react-cookie-consent";
 
 import { GA_TRACKING_ID } from "../lib/gtag";
 
@@ -35,20 +34,6 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-        <CookieConsent
-          debug={true}
-          location="bottom"
-          buttonText="Sure man!!"
-          cookieName="myAwesomeCookieName2"
-          style={{ background: "#2B373B" }}
-          buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
-          expires={150}
-        >
-          This website uses cookies to enhance the user experience.{" "}
-          <span style={{ fontSize: "10px" }}>
-            This bit of text is smaller :O
-          </span>
-        </CookieConsent>
       </Html>
     );
   }
