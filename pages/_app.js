d5f137e1406b609fa9194cadb07a3942020170e6
fhub-1 <flutterhub1.21.0@gmail.com>
2021-09-08 11:00:49 +0300

    adding new files

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,11 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-NW12Q5EQ1B`}
-      />
-
-      <Script strategy="lazyOnload">
-        {`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-NW12Q5EQ1B');`}
-      </Script>
-
-      <ThemeProvider defaultTheme="light" attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider defaultTheme="light" attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
 
